ff1529b8a8651378fa3d502e929d52aefa35d4a2
Geoffrey Ontiri <ontirischool@gmail.com>
2021-02-11 22:41:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} | #{board[3]} "
 end
 
 board[4] = "X"
